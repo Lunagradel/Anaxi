c7fd631cd6ef1daacd43c7df5c9d03d79b0ef739
Johannes Gade <jg@stromworks.com>
2017-12-12 12:08:42 +0100

    ...

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,24 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use DatabaseConnection;
+use AnaxiUser;
+use MongoDB\Client as MongoClient;
+use MongoDB\Collection as Collection;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function GetUsers(){
+	    $Database = new DatabaseConnection\DatabaseConnection();
+	    $Collection = $Database->GetMongoInstance();
+	    return $Collection->find()->toArray();
+    }
+
+    public function CreateUser( $request ) {
+	    $user = new AnaxiUser\User();
+	    $user->CreateUser($request);
+    }
+
 }
