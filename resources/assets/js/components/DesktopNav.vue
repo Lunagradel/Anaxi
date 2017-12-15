124ce616c0bf95ed82675946ea15e61c40dd30a8
lunagradel <lunig@hotmail.com>
2017-12-15 12:01:18 +0100

    added component createlocation.

--- resources/assets/js/components/DesktopNav.vue
+++ resources/assets/js/components/DesktopNav.vue
@@ -1,34 +1,62 @@
 <template>
-  <nav class="anaxi-nav">
-      <div class="anaxi-nav-content">
-          <router-link to="/" exact>
-              <div class="logo">
-                  <p>
-                      Anaxi
-                  </p>
-              </div>
-          </router-link>
-          <div class="anaxi-nav-content-btns">
-              <div class="anaxi-primary-btn" id="postBtn"><p>Post</p></div>
-              <i class="ion-android-globe"></i>
-              <router-link to="/profile" >
-                  <div class="profile-btn-content">
-                      <p class="profile-name">Anders</p>
-                      <div class="profile-avatar">
-                      </div>
-                      <div class="profile-arrow">
+    <div class="">
+        <createLocation
+        v-if="showCreate"
+        @showRecommend="showRecommend = true"
+        @closeLocation="showCreate = false"
+        ></createLocation>
+        <createRecommend
+        v-if="showRecommend"
+        @closeRecommend="showRecommend = false"
+        @showLocation="showCreate = true" 
+        ></createRecommend>
+        <nav class="anaxi-nav">
+            <div class="anaxi-nav-content">
+                <router-link to="/" exact>
+                    <div class="logo">
+                        <p>
+                            Anaxi
+                        </p>
+                    </div>
+                </router-link>
+                <div class="anaxi-nav-content-btns">
+                    <div class="anaxi-primary-btn" id="postBtn" v-on:click="showCreate = true"><p>Post</p></div>
+                    <i class="ion-android-globe"></i>
+                    <router-link to="/profile" >
+                        <div class="profile-btn-content">
+                            <p class="profile-name">Anders</p>
+                            <div class="profile-avatar"></div>
+                            <div class="profile-arrow"></div>
+                        </div>
+                    </router-link>
+                </div>
+            </div>
+        </nav>
+    </div>
 
-                      </div>
-                  </div>
-              </router-link>
-          </div>
-      </div>
-  </nav>
 </template>
 
 <script>
+
+import createLocation from './CreateLocation.vue';
+import createRecommend from './createRecommend.vue';
+
 export default {
-  mounted() {
+
+    data: function(){
+        return{
+            showCreate: false,
+            showRecommend: true
+        }
+    },
+
+    components: {
+        createLocation,
+        createRecommend
+    },
+
+
+    mounted() {
       console.log('Desktop Nav mounted.')
   }
 }
