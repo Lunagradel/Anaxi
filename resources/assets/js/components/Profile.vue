3f4714ed258bac9090e86a4f8d456a25d4358cbe
lunagradel <lunig@hotmail.com>
2017-12-19 10:56:25 +0100

    added map to profile page.

--- resources/assets/js/components/Profile.vue
+++ resources/assets/js/components/Profile.vue
@@ -1,5 +1,4 @@
 <template>
-    <div>
     <div class="anaxi-profile-page">
         <div class="anaxi-profile">
             <div class="anaxi-profile-background anaxi-profile-inner">
@@ -56,27 +55,27 @@
                         <div class="information-mobile">
                             <div class="information-item">
                                 <p>1. Lastname</p>
-                                <p>ANDERSEN</p>
+                                <p>{{lastName}}</p>
                             </div>
                             <div class="information-item">
                                 <p>2. Firstname</p>
-                                <p>ANDERS</p>
+                                <p>{{firstName}}</p>
                             </div>
                         </div>
                         <div class="information-item">
                             <p>3. Description</p>
-                            <p>heyo this is a description that is very long. maybe even two lines.</p>
+                            <p>{{description}}</p>
                         </div>
                     </div>
                     <div class="information-right">
                         <div class="information-mobile">
                             <div class="information-item">
                                 <p>4. Followers</p>
-                                <p>100</p>
+                                <p>{{followers}}</p>
                             </div>
                             <div class="information-item">
                                 <p>5. Following</p>
-                                <p>100</p>
+                                <p>{{following}}</p>
                             </div>
                         </div>
                         <div class="anaxi-primary-btn" id="profileBtn">
@@ -87,9 +86,12 @@
 
             </div>
         </div>
-    </div>
+        <div class="anaxi-profile-map" ref="profileMap">
 
-        <Experience v-for="(experience, index) in experiences" :key="index" v-bind:experience="experience"></Experience>
+        </div>
+        <div class="anaxi-profile-feed">
+            <Experience v-for="(experience, index) in experiences" :key="index" v-bind:experience="experience"></Experience>
+        </div>
     </div>
 
 
@@ -102,22 +104,130 @@ export default {
     Experience
   },
   data: function(){
-    return{
-      experiences: [],
+    return {
+        lastName: '',
+        firstName: '',
+        description: 'heyo this is a description that is very long. maybe even two lines.',
+        followers: 100,
+        following: 100,
+        experiences: [],
     }
+  },
+
+  methods: {
+      mapInit: function(){
+
+          //Google map
+          const bounds = new google.maps.LatLngBounds();
+          let mapName = this.$refs.profileMap;
+          const mapCenter = this.experiences[0].geolocation;
+          console.log(mapCenter);
+          let marker;
+
+          const options = {
+              center: new google.maps.LatLng(mapCenter.lat, mapCenter.lng),
+              zoom: 14,
+              mapTypeControl: false,
+              streetViewControl: false,
+              fullscreenControl: false,
+              styles: [
+                      {
+                          "featureType": "poi",
+                          "elementType": "geometry.fill",
+                          "stylers": [
+                              {
+                                  "color": "#5c9a8d"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "road",
+                          "elementType": "geometry",
+                          "stylers": [
+                              {
+                                  "lightness": 100
+                              },
+                              {
+                                  "visibility": "simplified"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "road",
+                          "elementType": "geometry.fill",
+                          "stylers": [
+                              {
+                                  "color": "#D1D1B8"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "water",
+                          "elementType": "geometry",
+                          "stylers": [
+                              {
+                                  "visibility": "on"
+                              },
+                              {
+                                  "color": "#5b7c8d"
+                              }
+                          ]
+                      },
+                      {
+                          "featureType": "water",
+                          "elementType": "labels.text.fill",
+                          "stylers": [
+                              {
+                                  "color": "#ffffff"
+                              }
+                          ]
+                      }
+                  ]
+          }
+
+          let map = new google.maps.Map(mapName, options);
+
+          //adding markers from the experiences array.
+          this.experiences.forEach((item, index)=> {
+              const position = new google.maps.LatLng(item.geolocation.lat, item.geolocation.lng);
+
+              marker = new google.maps.Marker({
+                  position,
+                  map
+              });
+
+              //when marker is pressed it calls toggleExperiences and shows the experience belonging to the marker.
+              google.maps.event.addListener(marker, 'click', function(){
+                  console.log("you clicked a marker with index: ", index );
+              });
+
+              //makes the zoom so that every marker is visible
+              map.fitBounds(bounds.extend(position));
+
+          });
+
+      }
   },
   mounted(){
     const self = this;
     axios.post('/getuserexperiences', this.$data)
       .then(function (response) {
-        console.log(response.data[0].experiences);
+        console.log(response.data[0]);
         self.experiences = response.data[0].experiences;
+        self.firstName = response.data[0].firstName;
+        self.lastName = response.data[0].lastName;
+
+        self.mapInit();
 //        this.$set('experiences', JSON.parse(response.data[0].experiences));
       })
       .catch(function (error) {
         console.log(error);
       });
+
+
   }
+
+
 
 }
 </script>
