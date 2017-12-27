fcec49ecd6ba164fb832cc22070fde4ef27e7d5d
lunagradel <lunig@hotmail.com>
2017-12-27 14:22:51 +0100

    added router links to the users showing on followers modal.

--- resources/assets/js/components/ShowFollowers.vue
+++ resources/assets/js/components/ShowFollowers.vue
@@ -9,7 +9,12 @@
             <div class="anaxi-follow-header" v-else>Followers</div>
             <div class="anaxi-follow-users">
                 <div class="anaxi-follow-user" v-for="(item, index) in this.followers" :key="index">
-                    <p>{{item.name}}</p>
+                    <router-link :to="{ name: 'profile', params: { id: item._id}}" v-if="typeIsFollowing">
+                        <p>{{item.name}}</p>
+                    </router-link>
+                    <router-link :to="{ name: 'profile', params: { id: item._id.$oid}}" v-else>
+                        <p>{{item.name}}</p>
+                    </router-link>
                 </div>
             </div>
         </div>
@@ -21,7 +26,14 @@
 export default {
     props: ['followers', 'following'],
 
+    data: function(){
+        return {
+            typeIsFollowing: this.following
+        }
+    },
+
     mounted: function(){
+        console.log("following:",this.followers);
         console.log("following:",this.following);
     }
 
