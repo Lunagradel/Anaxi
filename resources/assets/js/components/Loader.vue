beeb3653183f5d04929478d96912154c2e766b3d
lunagradel <lunig@hotmail.com>
2017-12-30 23:17:10 +0100

    added loader component and styled it.

---
+++ resources/assets/js/components/Loader.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div class="anaxi-loader">
+        <i class="ion-load-c spin"></i>
+        <p>Loading..</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
